e21ec37ee20b63f2b8b65296271e4577fbf6a868
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-14 17:13:35 +0600

    Voting

---
+++ contracts/Ballot.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ballot {
+    struct Voter {
+        uint256 weight;
+        bool voted;
+        address delegate; //The person voting to
+        uint256 vote; //voting proposal
+    }
+
+    struct Proposal {
+        string name;
+        uint256 voteCount;
+    }
+
+    address public chairperson;
+
+    mapping(address => Voter) public voters;
+
+    Proposal[] public proposals;
+
+    constructor(string[] memory proposalsName) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+
+        for (uint256 i = 0; i < proposalsName.length; i++) {
+            proposals.push(Proposal({name: proposalsName[i], voteCount: 0}));
+        }
+    }
+
+    function giveRightToVote(address voter) public {
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give the right."
+        );
+        require(!voters[voter].voted, "Can vote once");
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
+    }
+
+    function delegate(address to) public {
+        Voter storage sender = voters[msg.sender];
+
+        require(!sender.voted, "You already voted.");
+        require(to != msg.sender, "Self delegation is disallowed");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+
+        sender.voted = true;
+        sender.delegate = to;
+        Voter storage delegate_ = voters[to];
+
+        if (delegate_.voted) {
+            // If the delegate already voted,
+            // directly add to the number of votes
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            // If the delegate did not vote yet,
+            // add to her weight.
+            delegate_.weight += sender.weight;
+        }
+    }
+
+    function vote(uint256 proposal) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (string memory winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
---
+++ guidance/Ballot.md
@@ -0,0 +1,15 @@
+# Voting Contract
+
+## There is a chairperson who can give right to the voters(simple people) to vote
+
+## some voters(simple people) votes one person(P1) and others vote another person(P2)
+
+## P1 votes to one proposal(Boat) and P2 votes to another proposal(Paddy)
+
+**_Note : There may remain more than one proposals and person(P1,P2......)_**
+
+**_Rules_**
+
+1. One Person = > 1 vote
+2. That person can not vote who is being voted
+3. Only chairperson can give vote right
